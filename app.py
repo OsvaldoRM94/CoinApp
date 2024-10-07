8ef6d89d1a27cf934d6ea0965f6f61d65bb50930
Valdo94 <osvaldorodriguez1994@hotmail.com>
2024-10-06 22:00:39 -0600

    se agregan botones, sin conflictos

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-<<<<<<< HEAD
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 
@@ -10,6 +9,3 @@ if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
-=======
-st.write('Esta aplicación aún no es funcional. En construcción.')
->>>>>>> 1d3c12198b72436a1d1199c4fafa78ac6d0629cb
